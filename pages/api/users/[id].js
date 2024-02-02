854abc2d3b55d7031b35bd77cb82e08a34127107
Ahmad <scriptwithahmad@gmail.com>
2024-02-02 15:37:56 +0500

    remove specific address but not working

--- pages/api/users/[id].js
+++ pages/api/users/[id].js
@@ -1,40 +1,40 @@
-// import dbConnect from "@/config/dbConnect";
-// import userModal from "@/models/user";
+import dbConnect from "@/config/dbConnect";
+import userModal from "@/models/user";
 
-// export default async function handler(req, res) {
-//   dbConnect();
+export default async function handler(req, res) {
+  dbConnect();
 
-//   try {
-//     const fetchUser = await userModal.findById(req.query.id);
+  try {
+    const fetchUser = await userModal.findById(req.query.id);
 
-//     if (!fetchUser) {
-//       return res.status(404).json({
-//         success: false,
-//         message: "User Not Found!",
-//       });
-//     }
+    if (!fetchUser) {
+      return res.status(404).json({
+        success: false,
+        message: "User Not Found!",
+      });
+    }
 
-//     const deleteUser = await userModal.findByIdAndDelete(req.query.id);
+    const deleteUser = await userModal.findByIdAndDelete(req.query.id);
 
-//     if (!deleteUser) {
-//       return res.status(400).json({
-//         success: false,
-//         message: "Unable to delete the User!",
-//       });
-//     }
+    if (!deleteUser) {
+      return res.status(400).json({
+        success: false,
+        message: "Unable to delete the User!",
+      });
+    }
 
-//     res.status(200).json({
-//       success: true,
-//       message: "User Deleted Successfully!",
-//     });
-//   } catch (error) {
-//     res.status(500).json({
-//       success: false,
-//       message: "Server Error!",
-//       error: error.message,
-//     });
-//   }
-// }
+    res.status(200).json({
+      success: true,
+      message: "User Deleted Successfully!",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Server Error!",
+      error: error.message,
+    });
+  }
+}
 
 
 
@@ -114,66 +114,66 @@
 
 
 // 3rd logic ==================================================================/
-import dbConnect from "@/config/dbConnect";
-import userModal from "@/models/user";
+// import dbConnect from "@/config/dbConnect";
+// import userModal from "@/models/user";
 
-export default async function handler(req, res) {
-  dbConnect();
+// export default async function handler(req, res) {
+//   dbConnect();
 
-  try {
-    const userId = req.query.id;
-    const addressIdToDelete = req.query.addressId;
+//   try {
+//     const userId = req.query.id;
+//     const addressIdToDelete = req.query.addressId;
 
-    const fetchUser = await userModal.findById(userId);
+//     const fetchUser = await userModal.findById(userId);
 
-    if (!fetchUser) {
-      return res.status(404).json({
-        success: false,
-        message: "User Not Found!",
-      });
-    }
+//     if (!fetchUser) {
+//       return res.status(404).json({
+//         success: false,
+//         message: "User Not Found!",
+//       });
+//     }
 
-    if (addressIdToDelete) {
-      // If addressId is provided, delete the specified addressDetails object
-      const updatedUser = await userModal.findByIdAndUpdate(
-        userId,
-        { $pull: { addressDetails: { _id: addressIdToDelete } } },
-        { new: true } // to get the updated user document
-      );
+//     if (addressIdToDelete) {
+//       // If addressId is provided, delete the specified addressDetails object
+//       const updatedUser = await userModal.findByIdAndUpdate(
+//         userId,
+//         { $pull: { addressDetails: { _id: addressIdToDelete } } },
+//         { new: true } // to get the updated user document
+//       );
 
-      if (!updatedUser) {
-        return res.status(404).json({
-          success: false,
-          message: "AddressDetails Not Found!",
-        });
-      }
+//       if (!updatedUser) {
+//         return res.status(404).json({
+//           success: false,
+//           message: "AddressDetails Not Found!",
+//         });
+//       }
 
-      res.status(200).json({
-        success: true,
-        message: "AddressDetails Deleted Successfully!",
-      });
-    } else {
-      // If addressId is not provided, delete the entire user
-      const deleteUser = await userModal.findByIdAndDelete(userId);
+//       res.status(200).json({
+//         success: true,
+//         message: "AddressDetails Deleted Successfully!",
+//       });
+//     } else {
+//       // If addressId is not provided, delete the entire user
+//       const deleteUser = await userModal.findByIdAndDelete(userId);
 
-      if (!deleteUser) {
-        return res.status(400).json({
-          success: false,
-          message: "Unable to delete the User!",
-        });
-      }
+//       if (!deleteUser) {
+//         return res.status(400).json({
+//           success: false,
+//           message: "Unable to delete the User!",
+//         });
+//       }
 
-      res.status(200).json({
-        success: true,
-        message: "User Deleted Successfully!",
-      });
-    }
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: "Server Error!",
-      error: error.message,
-    });
-  }
-}
+//       res.status(200).json({
+//         success: true,
+//         message: "User Deleted Successfully!",
+//       });
+//     }
+//   } catch (error) {
+//     res.status(500).json({
+//       success: false,
+//       message: "Server Error!",
+//       error: error.message,
+//     });
+//   }
+// }
 
